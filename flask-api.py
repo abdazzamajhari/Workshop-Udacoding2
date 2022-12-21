913dadfb93681372be6924798d7a374bc88b549d
Abdul Azzam Ajhari <42313402+abdazzamajhari@users.noreply.github.com>
2022-12-21 07:56:43 +0700

    Update flask-api.py

--- flask-api.py
+++ flask-api.py
@@ -31,4 +31,4 @@ def home():
         # return render_template('home.html', result = result)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9000)
+    app.run(host='0.0.0.0', port=5000)
